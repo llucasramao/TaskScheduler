3aa6c5e90b4f9f28930eed41e0956dcb5167426b
Lucas - SRV02 <ramaolucas01@gmail.com>
2022-02-12 22:24:22 +0000

    Atualizado

--- infra/conexao.js
+++ infra/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'mysql',
+    password: 'S3c_Mysql',
     database: 'nodejs'
 })
 
