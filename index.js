c3588437bb806c73042ee9849570cf04ee4ef98c
Lucas Ramão <ramaolucas01@gmail.com>
2022-02-17 18:18:40 -0300

    add

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const Tableas = require('./infra/tabelas')
 const https = require('https')
 const fs = require('fs')
 
-ssl = 0
+const ssl = 0;
 if (ssl == 0){http()}
 if (ssl == 1){httpsec()}
 else{console.log('Defina o ssl no index.js (0 para não | 1 para sim)')}
-
-
 
 function http(){
     conexao.connect(erro => {
@@ -18,14 +16,11 @@ function http(){
         } else {
             console.log('MySQL Conectado!')
             Tableas.init(conexao)
-
             const app = customExpress()
             app.listen(3000, () => console.log('API HTTP rodando na porta 3000'))
-
         }
     })
 }
-
 
 function httpsec(){
     conexao.connect(erro => {
@@ -34,13 +29,10 @@ function httpsec(){
         } else {
             console.log('MySQL Conectado!')
             Tableas.init(conexao)
-
-
             const options = {
                 key: fs.readFileSync("./certificates/privkey.pem"),
                 cert: fs.readFileSync("./certificates/cert.pem")
                 };
-
             const app = customExpress()
             https.createServer(options, app).listen(3000, () => console.log('API HTTPS rodando na porta 3000'));
         }
--- models/Tasks.js
+++ models/Tasks.js
@@ -103,20 +103,6 @@ class Tasks {
         })
     }
 
-    sendMessage(msg, res, client, number, message){
-        const sql = `INSERT INTO messages (client, number, message, date) VALUES ('${client}', '${number}', '${message}', ADDDATE(NOW(), INTERVAL 0 DAY) )` //ADDDATE(NOW(), INTERVAL 0 DAY)
-        conexao.query(sql, msg, (err, result) => {
-            if (err) {
-                res.status(400).json(err)
-                console.log('erro')
-            } else {
-                res.status(201).json(result)
-            }
-        })
-
-        sendMsg(client, number, message)
-    }
-
     schMessage(msg, res, client, number, message, date){
         let date2 = moment(date, 'DD/MM/YYYY HH').format('YYYY-MM-DD HH')
         const sql = `INSERT INTO schMessage (client, number, message, date) VALUES ('${client}', ${number}, '${message}', '${date2}')`
@@ -129,11 +115,50 @@ class Tasks {
         })
     }
 
-}
+    sendMessage(msg, res, client, number, message){
+        const sql = `INSERT INTO messages (client, number, message, date) VALUES ('${client}', '${number}', '${message}', ADDDATE(NOW(), INTERVAL 0 DAY) )` //ADDDATE(NOW(), INTERVAL 0 DAY)
+        conexao.query(sql, msg, (err, result) => {
+            if (err) {
+                res.status(400).json(err)
+                console.log('erro')
+            } else {
+                res.status(200).json(result)
+            }
+        })
 
+        sendMsg(client, number, message)
+    }
+}
 
 module.exports = new Tasks
 
 function sendMsg(client, number, message){
-    console.log(`Envio de mensagem para ${client}, ${number}, ${message}`)
+    var axios = require('axios');
+    var data = JSON.stringify({
+    "phone": number,
+    "message": message,
+    "isGroup": false
+    });
+
+    var config = {
+    method: 'post',
+    url: `http://api.spacewebso.com.br:2121/api/Lucas/send-message`,
+    headers: { 
+        'Content-Type': 'application/json', 
+        'Authorization': `Bearer TOKEN-AQUI!`
+    },
+    data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+    if (response.data.status == 'success'){
+        console.log('Mensagem Enviada -', `Numero: ${number} Msg: ${message}`)
+    } else {
+        console.log('Erro: response-not-success')
+    }
+    })
+    .catch(function (error) {
+    console.log('Erro: catch-function-error');
+    });
 }
