c53c7d3eca669aa3b2d9c4614dc7a84692acb362
Lucas Araújo <ramaolucas01@gmail.com>
2022-02-18 16:32:05 -0300

    add

--- infra/conexao.js
+++ infra/conexao.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql')
 
-const conexao = mysql.createConnection({
+const conexao = mysql.createConnection(
+{
     host: 'localhost',
     port: '3306',
     user: 'root',
     password: 'mysql',
     database: 'nodejs'
-})
+}
+)
 
 module.exports = conexao
